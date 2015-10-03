65bd080a77fbf9cb6c47a7776cd7e26d096bfe06
Dan Murphy <murphydbuffal@gmail.com>
2015-10-03 19:36:11 -0400

    Ensure input is a string, handle non-alphanumerics
    Added a couple functions to `cipher.js` for use in `encrypt()`. The first checks that the input
    passed to `encrypt()` is a string, the second checks that a given string is an alphanumeric
    character.
    
    If the input passed to `encrypt()` is not a string, a blank string is returned. `encrypt()` should
    always return a string in case it is being used as part of a larger system that relies upon
    its return value being a string.
    
    If a character in the string input is non-alphanumeric `encrypt()` does not encrypt that character,
    and instead adds it as-is to the encrypted string.
    
    Unit tests added for these scenarios.

--- cipher.js
+++ cipher.js
@@ -1,6 +1,10 @@
 const alphabet = '0123456789abcdefghijklmnopqrstuvwxyz'.split('');
+const isAlphaNumeric = string => (/[a-z0-9]/i).test(string);
+const isString = input => typeof input === 'string';
 
 exports.encrypt = function encrypt(input, shift) {
+  if(!isString(input)) return '';
+
   shift = (shift == null ? 3 : shift);
 
   const mapOfCharacters = alphabet.reduce((map, char, index, array) => {
@@ -17,8 +21,10 @@ exports.encrypt = function encrypt(input, shift) {
     return map;
   }, {});
 
-  const encryptedString = String(input).split('').reduce((result, character) => {
-    return result + mapOfCharacters[character];
+  const encryptedString = input.split('').reduce((result, character) => {
+    const encryptedChar = (isAlphaNumeric(character) ? mapOfCharacters[character] : character);
+
+    return result + encryptedChar;
   }, '');
 
   return encryptedString;
--- test.js
+++ test.js
@@ -10,19 +10,39 @@ describe('The Caesar cipher', () => {
       encrypt('foo').should.equal('irr');
       encrypt('abc').should.equal('def');
       encrypt('zzz').should.equal('222');
-      encrypt(123).should.equal('456');
+      encrypt('123').should.equal('456');
     });
 
     it('should accept an optional "shift" parameter', () => {
       encrypt('foo', 7).should.equal('mvv');
       encrypt('abc', 7).should.equal('hij');
       encrypt('yyy', 7).should.equal('555');
-      encrypt(123, 7).should.equal('89a');
+      encrypt('123', 7).should.equal('89a');
     });
 
-    it('should not attempt to encrypt non-alphanumeric characters');
-    it('should return a blank string when given a blank string');
-    it('should return a blank string when given a non-string or numeric value');
+    it('should not attempt to encrypt non-alphanumeric characters', () => {
+      encrypt('foo!').should.equal('irr!');
+      encrypt('ab?c').should.equal('de?f');
+      encrypt('-_|').should.equal('-_|');
+      encrypt('*&^%').should.equal('*&^%');
+      encrypt('     ').should.equal('     ');
+      encrypt('\n\n').should.equal('\n\n');
+      encrypt('\t').should.equal('\t');
+    });
+
+    it('should return a blank string when given a blank string', () => {
+      encrypt('').should.equal('');
+    });
+
+    it('should return a blank string when given a non-string value', () => {
+      encrypt(null).should.equal('');
+      encrypt(undefined).should.equal('');
+      encrypt(123).should.equal('');
+      encrypt(true).should.equal('');
+      encrypt(false).should.equal('');
+      encrypt(['a', 'b', 'c']).should.equal('');
+      encrypt({ a: 1, b: 2, c: 3 }).should.equal('');
+    });
   });
 
   describe('The decrypt function', () => {
