e9cfb732af1405fc548f6f0a4484b55062f3947c
Dan Murphy <murphydbuffal@gmail.com>
2015-10-03 18:59:57 -0400

    Failing specs for encrypting alphanumeric input.

--- test.js
+++ test.js
@@ -1,13 +1,21 @@
 const chai = require('chai');
+const encrypt = require('./cipher').encrypt;
 
 // Use BDD style "foo.should.equal('bar')" assertions
 chai.should();
 
 describe('The Caesar cipher', () => {
   describe('The encrypt function', () => {
-    it('should return an encrypted string when given alphanumeric characters');
-    it('should return a blank string when given a blank string');
+    it('should return an encrypted string when given alphanumeric characters', () => {
+      encrypt('foo').should.equal('irr');
+      encrypt('abc').should.equal('def');
+      encrypt('zzz').should.equal('222');
+      encrypt(123).should.equal('456');
+    });
+
     it('should not attempt to encrypt non-alphanumeric characters');
+    it('should return a blank string when given a blank string');
+    it('should return a blank string when given a non-string or numeric value');
   });
 
   describe('The decrypt function', () => {
